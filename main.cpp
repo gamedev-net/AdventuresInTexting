761905001015a87c9a91f16cb08f76f1b83c9d0c
TheComet <alex.murray@gmx.ch>
2017-04-22 00:49:15 +0200

    Slowly expanding blessed sword, until it becomes readable

--- main.cpp
+++ main.cpp
@@ -106,7 +106,9 @@ struct HealthItem : public Item
 private:
     int healPower;
 };
-struct BlessedSword : public Item{BlessedSword(int Weapon) : Weapon(Weapon){}
+struct BlessedSword : public Item
+{
+    BlessedSword(int Weapon) : Weapon(Weapon){}
 void apply(Entity* entity) override{entity->damage(Weapon);}string identify() const override{stringstream ss; ss << "Hit (" << Weapon << ")";}
 private: int Weapon;};//will add this to entity on my next commit. <.<
 
